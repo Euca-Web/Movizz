75ab02799e908105d1087ca32a25042af9d30046
euca <eucawebdev@gmail.com>
2024-12-12 16:04:28 +0100

    Lien entre API et react

--- code/client/src/page/ContactPage.tsx
+++ code/client/src/page/ContactPage.tsx
@@ -1,4 +1,11 @@
+import MovizzList from "../component/home/MovizzList";
+
 const ContactPage = () => {
-	return <h1>Contact</h1>;
+	return (
+		<>
+			<h1>Contact</h1>
+			<MovizzList />
+		</>
+	);
 };
 export default ContactPage;
--- code/server/src/core/server.ts
+++ code/server/src/core/server.ts
@@ -1,5 +1,10 @@
 import http from "node:http";
-import express, { type Router, type Express, type Request, type Response } from "express";
+import express, {
+	type Router,
+	type Express,
+	type Request,
+	type Response,
+} from "express";
 import HomePageRouter from "../router/homepage_routeur.js";
 import genderRouter from "../router/gender_routeur.js";
 import NotFoundRouter from "../router/not_found_router.js";
@@ -8,37 +13,38 @@ import directorRouter from "../router/director_routeur.js";
 import actorRouter from "../router/actor_routeur.js";
 import roleRouter from "../router/role_routeur.js";
 import userRouter from "../router/user_routeur.js";
-
+import cors from "cors";
 
 class Server {
-// instancier une application Express
-    private app: Express = express();
-// définir un routeur pour Express
-    private router: Router = express.Router();
-constructor() {
-// lier l'application Express au routeur
-    this.app.use(this.router);
-// définir la liste des routeurs
-    this.getRoutersList();
-}
-// liste des routeurs
-    private getRoutersList = (): void => {
-// création de la route d'accueil en GET
-        this.router.use('/', new HomePageRouter().getRoutes());
-        this.router.use('/gender', new genderRouter().getRoutes());
-        this.router.use('/movie', new movieRouter().getRoutes());
-        this.router.use('/director', new directorRouter().getRoutes());
-        this.router.use('/actor', new actorRouter().getRoutes());
-        this.router.use('/role', new roleRouter().getRoutes());
-        this.router.use('/user', new userRouter().getRoutes());
+	// instancier une application Express
+	private app: Express = express();
+	// définir un routeur pour Express
+	private router: Router = express.Router();
+	constructor() {
+		// gérer les requêtes multi-origine: cors = Cross Origin Ressource Sharing
+		this.app.use(cors());
+		// lier l'application Express au routeur
+		this.app.use(this.router);
+		// définir la liste des routeurs
+		this.getRoutersList();
+	}
+	// liste des routeurs
+	private getRoutersList = (): void => {
+		// création de la route d'accueil en GET
+		this.router.use("/", new HomePageRouter().getRoutes());
+		this.router.use("/gender", new genderRouter().getRoutes());
+		this.router.use("/movie", new movieRouter().getRoutes());
+		this.router.use("/director", new directorRouter().getRoutes());
+		this.router.use("/actor", new actorRouter().getRoutes());
+		this.router.use("/role", new roleRouter().getRoutes());
+		this.router.use("/user", new userRouter().getRoutes());
 
-
-    // Routeur des routes inexistantes doit être OBLIGATOIREMENT en dernière position
-    this.router.use('*', new NotFoundRouter().getRoutes());
-};
-// créer un serveur Node.js / Express
-public createServer = (): http.Server => {
-    return http.createServer(this.app);
-};
+		// Routeur des routes inexistantes doit être OBLIGATOIREMENT en dernière position
+		this.router.use("*", new NotFoundRouter().getRoutes());
+	};
+	// créer un serveur Node.js / Express
+	public createServer = (): http.Server => {
+		return http.createServer(this.app);
+	};
 }
 export default Server;
